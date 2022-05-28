76cd50d6b0ffa7ac2080fe9ca72ebf04c83162d2
Rocio <roaguerrero17@gmail.com>
2022-05-27 21:29:07 -0300

    creacion proyecto

---
+++ src/main/java/ar/edu/unju/fi/tpevaluativo_grupo08/TpEvaluativoGrupo08Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpevaluativo_grupo08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpEvaluativoGrupo08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpEvaluativoGrupo08Application.class, args);
+    }
+
+}
---
+++ src/test/java/ar/edu/unju/fi/tpevaluativo_grupo08/TpEvaluativoGrupo08ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpevaluativo_grupo08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpEvaluativoGrupo08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
