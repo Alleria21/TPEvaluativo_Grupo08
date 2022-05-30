71dcfe55064d3c6a12d7fd547f21b575ca7a175d
argca <argca@LAPTOP-AUEUA7T3>
2022-05-30 01:45:29 -0300

    agregando servicio imp

---
+++ src/main/java/ar/edu/unju/fi/tpevaluativo_grupo08/service/imp/CandidatoServiceImp.java
@@ -0,0 +1,84 @@
+package ar.edu.unju.fi.tpevaluativo_grupo08.service.imp;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.tpevaluativo_grupo08.model.Candidato;
+import ar.edu.unju.fi.tpevaluativo_grupo08.service.ICandidatoService;
+import ar.edu.unju.fi.tpevaluativo_grupo08.util.ListaCandidato;
+
+
+
+@Service("CandidatoServiceImpList")
+public class CandidatoServiceImp implements ICandidatoService {
+
+	@Autowired
+	private ListaCandidato listaCandidato;
+	
+	@Override
+	public Candidato getCandidato() {
+		return new Candidato();
+	}
+
+	@Override
+	public boolean guardarCandidato(Candidato candidato) {
+		boolean respuesta=true;
+		for(Candidato can: listaCandidato.getCandidato()) {
+			if(can.getCodigo()==candidato.getCodigo()) {
+				respuesta=false;
+			}
+		}
+		if(respuesta==true) {
+			candidato.setVotos(0);
+			listaCandidato.getCandidato().add(candidato);
+		}
+		return respuesta;
+	}
+
+	@Override
+	public ListaCandidato getListaCandidato() {
+		// TODO Auto-generated method stub
+		return listaCandidato;
+	}
+	@Override
+	public Candidato buscarCandidato(int codigo) {
+		// Busca un alumno por dni
+		Optional<Candidato> candidato=listaCandidato.getCandidato().stream().filter(a -> a.getCodigo() == codigo).findFirst();
+		return candidato.get();
+	}
+	@Override
+	public void modificarCandidato(Candidato candidato) {
+		for(Candidato can: listaCandidato.getCandidato()) {
+			if(can.getCodigo()==candidato.getCodigo())
+			{
+				can.setNombre(candidato.getNombre());
+				can.setGenero(candidato.getGenero());
+				can.setDescripcion(candidato.getDescripcion());
+			}
+		}
+	}
+	@Override
+	public void eliminarCandidato(int codigo) {
+		int indice=0;
+		for(int i=0;i<listaCandidato.getCandidato().size();i++) {
+			if(listaCandidato.getCandidato().get(i).getCodigo()==codigo	)
+			{
+				indice=i;
+			}
+		}
+		listaCandidato.getCandidato().remove(indice);
+	}
+
+	@Override
+	public void guardarVoto(int codigo) {
+		for(Candidato can: listaCandidato.getCandidato()) {
+			if(can.getCodigo()==codigo)
+			{
+				can.setVotos(can.getVotos()+1);
+			}
+		}
+	}
+
+}
---
+++ src/main/java/ar/edu/unju/fi/tpevaluativo_grupo08/service/imp/UsuarioServiceImp.java
@@ -0,0 +1,58 @@
+package ar.edu.unju.fi.tpevaluativo_grupo08.service.imp;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.tpevaluativo_grupo08.model.Usuario;
+import ar.edu.unju.fi.tpevaluativo_grupo08.service.IUsuarioService;
+import ar.edu.unju.fi.tpevaluativo_grupo08.util.ListaUsuario;
+
+
+
+@Service("UsuarioServiceImpList")
+public class UsuarioServiceImp implements IUsuarioService {
+
+	@Autowired
+	private ListaUsuario listaUsuario;
+	
+	@Override
+	public Usuario getUsuario() {
+		// retornar un objeto de la clase Usuario
+		return new Usuario();
+	}
+
+	@Override
+	public boolean guardarUsuario(Usuario usuario) {
+		// guardar un objeto
+		boolean respuesta=listaUsuario.getUsuario().add(usuario);//Controlar que no se agreguen repetidos
+		return respuesta;
+	}
+
+	@Override
+	public ListaUsuario getListaUsuario() {
+		// TODO Auto-generated method stub
+		return listaUsuario;
+	}
+	
+	@Override
+	public boolean guardarVoto(String mail) {
+		boolean respuesta=false;
+		for(Usuario usu: listaUsuario.getUsuario()){
+			if(usu.getEmail().equals(mail)) {
+				usu.setCont_voto(usu.getCont_voto()+1);
+				respuesta=true;
+			}
+		}
+		return respuesta;
+	}
+	@Override
+	public Usuario buscarUsuario(String email) {
+		Optional<Usuario> usuario=listaUsuario.getUsuario().stream().filter(a -> a.getEmail().equals(email)).findFirst();
+		return usuario.get();
+	}
+
+
+}
